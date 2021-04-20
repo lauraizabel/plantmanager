4c2f9d302f99e7778a149a1da4bb75a737be6007
Laura Izabel <lauraizabel8569@gmail.com>
2021-04-20 20:18:15 -0300

    finish welcome

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import Welcome from "./src/pages/Welcome";
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_600SemiBold,
+    Jost_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Welcome />;
 }
--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,25 +1,42 @@
 import React from "react";
 
-import { SafeAreaView, Text, StyleSheet, Image } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Image,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  StatusBar,
+} from "react-native";
 import colors from "../../../styles/colors";
+import fonts from "../../../styles/fonts";
+
+import { Feather } from "@expo/vector-icons";
 
 import wateringImg from "../../assets/watering.png";
-import Button from "../../components/Button";
 
 function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {"\n"} suas plantas {"\n"} de forma fácil
-      </Text>
-      <Image source={wateringImg} style={styles.image} />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"} suas plantas de {"\n"} forma fácil
+        </Text>
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas{"\n"} plantas. Nós cuidamos de lembrar
-        você {"\n"}sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas{"\n"} plantas. Nós cuidamos de lembrar
+          você sempre que precisar.
+        </Text>
 
-      <Button title={">"} />
+        <View>
+          <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+            <Feather name="chevron-right" style={styles.buttonIcon} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -27,26 +44,44 @@ function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
     paddingTop: 64,
   },
+  wrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
   title: {
-    fontWeight: "bold",
-    fontSize: 32,
+    fontSize: 28,
     color: colors.heading,
     textAlign: "center",
     marginTop: 12,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle: {
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   image: {
-    width: 292,
-    height: 284,
+    width: Dimensions.get("window").width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 42,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32,
   },
 });
 
